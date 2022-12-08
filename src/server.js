adf7859da666d3591a1b9665954b30972e3a9b8d
BloodShop <koliakovcr7@gmail.com>
2022-12-08 16:23:56 +0200

    logger-specific

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', { text: 'wazzzap' })
+    res.render('index', { text: 'wazzzap' });
     // res.download('src/index.js') // Donwload the file
     // res.status(500).json({ message: 'Error' })
 })
 
-const userRouter = require('../routes/users')
-app.use('/users', userRouter)
+/* app.use(logger); // Going to be invoked only when the routes below are called, not the above */
 
-function logger(req, res, next) {
+const userRouter = require('../routes/users');
+app.use('/users', userRouter);
 
-}
+/* function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
 
-app.listen(3000)
+} */
+
+app.listen(3000);
